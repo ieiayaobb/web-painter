0f7c3d3567dfdfd6ac572faa998a4ef6ff2991c5
ieiayaobb <ieiayaobb@gmail.com>
2015-06-23 11:45:50 +0800

    rename

--- js/customLine.js
+++ js/customLine.js
@@ -1,8 +1,129 @@
 /**
  * Created by shenyineng on 15/6/19.
  */
-define(["zrender/shape/Line"],
-    function(Line){
-        return Line;
+define(
+    ["zrender/shape/Line", "zrender/tool/util", "CustomPoint", "CustomBezier"],
+    function(Line, util, CustomPoint, CustomBezier){
+        var CustomLine = function(options){
+            var _this = this;
+
+            // bind point dragging event
+            function bindDrag(){
+                var currentPoint = this;
+                window.zr.on('mousemove', function(params){
+                    var targetX = params.event.offsetX;
+                    var targetY = params.event.offsetY;
+                    currentPoint.style.x = targetX
+                    currentPoint.style.y = targetY
+                    zr.modShape(currentPoint);
+                    if(currentPoint == _startPoint){
+                        _this.style.xStart = targetX
+                        _this.style.yStart = targetY;
+                    }else if(currentPoint == _endPoint){
+                        _this.style.xEnd = targetX
+                        _this.style.yEnd = targetY;
+                    }
+
+                    zr.modShape(_this)
+                });
+            }
+
+            function drawLine(){
+                window.zr.on('mousemove', function(params){
+                    var targetX = params.event.offsetX;
+                    var targetY = params.event.offsetY;
+                    _startPoint.style.x = targetX
+                    _startPoint.style.y = targetY
+                    window.zr.modShape(_startPoint)
+                    _this.style.xStart = targetX
+                    _this.style.yStart = targetY;
+                    window.zr.modShape(_this)
+                });
+            }
+
+
+            // start point
+            var _startPoint = options.startPoint;
+            // end point
+            var _endPoint = options.endPoint;
+
+            console.log("startPoint : " + _startPoint.x + "," + _startPoint.y);
+            console.log("endPoint : " + _endPoint.x + "," + _endPoint.y);
+
+            _startPoint.onmousedown = bindDrag;
+            _endPoint.onmousedown = bindDrag;
+
+            _startPoint.onmouseup = function(params){
+                _startPoint.onmousemove = null;
+            };
+
+            _endPoint.onmouseup = function(params){
+                _startPoint.onmousemove = null;
+            };
+
+            options.style = {
+                xStart: _startPoint.x,
+                yStart: _startPoint.y,
+                xEnd: _endPoint.x,
+                yEnd: _endPoint.y,
+                strokeColor: 'black',
+                lineWidth: 2
+            }
+            options.onmousedown = drawLine;
+            options.onmouseup = function(params){
+                onmousemove = null;
+            }
+
+            options.clickable = true,
+            options.ondblclick = function(params){
+                var targetX = params.event.offsetX;
+                var targetY = params.event.offsetY;
+
+                //remove original line
+                window.zr.delShape(_this)
+
+                // new point at your mouse position
+                var p_new = new CustomPoint({
+                    x : targetX,
+                    y : targetY
+                });
+
+                window.zr.addShape(p_new);
+
+                var p_anchor1 = new CustomPoint(
+                    Triangle.findAnchor(p_new.style.x, p_new.style.y, _endPoint.x, _endPoint.y, 50)
+                );
+                //window.zr.addShape(p_anchor1);
+
+                var line_anchor1 = new Line({
+                    startPoint : this.p_new,
+                    endPoint : this.p_anchor1
+                });
+                //window.zr.addShape(line_anchor1);
+
+
+                // start to new
+                var halfLine1 = new CustomLine({
+                    startPoint : _startPoint,
+                    endPoint : p_new
+                });
+                window.zr.addShape(halfLine1);
+
+                // new to end
+                var halfLine2 = new CustomLine({
+                    startPoint : p_new,
+                    endPoint : _endPoint
+                });
+                window.zr.addShape(halfLine2);
+            }
+
+            Line.call(this, options);
+        }
+
+
+        util.inherits(CustomLine, Line);
+        
+        
+        return CustomLine;
     }
 )
--- js/customPoint.js
+++ js/customPoint.js
@@ -1,8 +1,73 @@
 /**
  * Created by shenyineng on 15/6/19.
  */
-define(["zrender/shape/Circle"],
-    function(Line){
-        return Line;
+define(
+    ["zrender/shape/Circle", "zrender/tool/util"],
+    function(Circle, util){
+        var CustomPoint = function(options){
+            var x = options.x;
+            var y = options.y;
+            options.style = {
+                x : x,
+                y : y,
+                r : 5,
+                color : "white",
+                brushType : "both",
+                strokeColor : "black",
+                lineWidth : 1
+            }
+
+            function setAsNew(){
+                options.draggable = true;
+                options. onmousedown = function(params){
+                    window.zr.on('mousemove', function(params){
+
+                        // mouse position
+                        this.targetX = params.event.offsetX;
+                        this.targetY = params.event.offsetY;
+
+                        // calculate offset
+                        this.offsetX = _this.halfLine1.style.xEnd - this.targetX;
+                        this.offsetY = _this.halfLine1.style.yEnd - this.targetY;
+
+                        // set anchor1
+                        _this.p_anchor1.style.x -= this.offsetX;
+                        _this.p_anchor1.style.y -= this.offsetY;
+                        window.zr.modShape(_this.p_anchor1);
+
+                        _this.line_anchor1.style.xEnd = this.targetX;
+                        _this.line_anchor1.style.yEnd = this.targetY;
+                        _this.line_anchor1.style.xStart = _this.p_anchor1.style.x;
+                        _this.line_anchor1.style.yStart = _this.p_anchor1.style.y;
+
+                        _this.halfLine1.style.cpX1 = _this.p_anchor1.style.x;
+                        _this.halfLine1.style.cpY1 = _this.p_anchor1.style.y;
+                        _this.halfLine1.style.xEnd = this.targetX
+                        _this.halfLine1.style.yEnd = this.targetY
+
+                        // set anchor1
+                        _this.p_anchor2.style.x -= this.offsetX;
+                        _this.p_anchor2.style.y -= this.offsetY;
+                        window.zr.modShape(_this.p_anchor2);
+
+                        _this.line_anchor2.style.xEnd = this.targetX;
+                        _this.line_anchor2.style.yEnd = this.targetY;
+                        _this.line_anchor2.style.xStart = _this.p_anchor2.style.x;
+                        _this.line_anchor2.style.yStart = _this.p_anchor2.style.y;
+
+                        _this.halfLine2.style.cpX1 = _this.p_anchor2.style.x;
+                        _this.halfLine2.style.cpY1 = _this.p_anchor2.style.y;
+                        _this.halfLine2.style.xEnd = this.targetX
+                        _this.halfLine2.style.yEnd = this.targetY
+                    });
+                }
+            }
+
+            Circle.call(this, options);
+        }
+
+        util.inherits(CustomPoint, Circle);
+
+        return CustomPoint;
     }
 )
